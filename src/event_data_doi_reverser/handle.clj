6dd41f0c170bb089490e26f5ddf17cff1b80d7d1
Joe Wass <jwass@crossref.org>
2016-09-29 10:58:46 +0300

    Add headers to Handle API communication.

--- src/event_data_doi_reverser/handle.clj
+++ src/event_data_doi_reverser/handle.clj
@@ -8,13 +8,23 @@
   (:gen-class))
 
 
+(def headers {
+  "User-Agent" "Crossref Event Data labs@crossref.org (+http://eventdata.crossref.org)"
+  "Referer" "http://eventdata.crossref.org/bot"})
+
 (defn resolve-doi-link-async
   "Resolve the URL from the DOI to find the resource url or the alias DOI. 
   Return tuple [resource-url alias-doi]. Only one is non-nil."
   [doi]
   (future
     (let [url (str "http://doi.org/api/handles/" (URLEncoder/encode doi "UTF-8"))
-          values (try-try-again {:sleep 5000 :tries 10} #(-> url http/get deref :body (json/read-str :key-fn keyword) :values))
+          values (try-try-again {:sleep 5000 :tries 10}
+                                #(-> url
+                                     (http/get {:headers headers})
+                                     deref
+                                     :body
+                                     (json/read-str :key-fn keyword)
+                                     :values))
           alias-value (-> (filter #(= (:type %) "HS_ALIAS") values) first :data :value)
           url-value (-> (filter #(= (:type %) "URL") values) first :data :value)]
       (if alias-value
